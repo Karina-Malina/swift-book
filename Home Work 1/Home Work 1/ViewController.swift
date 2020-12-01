7c751986e29e11b0713bc8ebf4f1fa869045ad36
Karina Blinova <blinova.k@ati.su>
2020-12-01 16:00:06 +0300

    HW2.1 part 1

---
+++ Home Work 1/Home Work 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Work 1
+//
+//  Created by Karina Blinova on 01.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
